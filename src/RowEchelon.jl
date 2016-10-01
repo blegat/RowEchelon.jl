34f6fcc74f7db7b853c2cd954b1660afe9176755
Benoît Legat <benoit.legat@gmail.com>
2016-10-01 19:42:43 +0200

    Copy Paste rref from Julia Base

--- src/RowEchelon.jl
+++ src/RowEchelon.jl
@@ -1,5 +1,39 @@
 module RowEchelon
 
-# package code goes here
+export rref
+
+function rref{T}(A::Matrix{T})
+    nr, nc = size(A)
+    U = copy!(similar(A, T <: Complex ? Complex128 : Float64), A)
+    ɛ = eps(norm(U,Inf))
+    i = j = 1
+    while i <= nr && j <= nc
+        (m, mi) = findmax(abs(U[i:nr,j]))
+        mi = mi+i - 1
+        if m <= ɛ
+            U[i:nr,j] = 0
+            j += 1
+        else
+            for k=j:nc
+                U[i, k], U[mi, k] = U[mi, k], U[i, k]
+            end
+            d = U[i,j]
+            for k = j:nc
+                U[i,k] /= d
+            end
+            for k = 1:nr
+                if k != i
+                    d = U[k,j]
+                    for l = j:nc
+                        U[k,l] -= d*U[i,l]
+                    end
+                end
+            end
+            i += 1
+            j += 1
+        end
+    end
+    U
+end
 
 end # module
