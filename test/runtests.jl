6ad4a929316ba5fb59c62aa951c39a607060bae0
Benoît Legat <benoit.legat@gmail.com>
2016-10-01 19:43:06 +0200

    Add basic tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,32 @@
 using RowEchelon
-using Base.Test
+using FactCheck
 
-# write your own tests here
-@test 1 == 2
+facts("Example from Wikipedia") do
+    A = [1 3 -1;
+         0 1  7]
+    R = [1 0 -22;
+         0 1   7]
+    @fact isapprox(rref(A), R) --> true
+end
+
+facts("Example from Rosetta Code") do
+    A = [ 1   2   -1   -4;
+          2   3   -1   -11;
+         -2   0   -3    22]
+    R = [ 1   0   0   -8;
+          0   1   0    1;
+          0   0   1   -2]
+    @fact isapprox(rref(A), R) --> true
+end
+
+facts("Magic Square") do
+    A = [16  2   3  13;
+         5  11  10   8;
+         9   7   6  12;
+         4  14  15   1]
+    R = [1   0   0   1;
+         0   1   0   3;
+         0   0   1  -3;
+         0   0   0   0]
+    @fact isapprox(rref(A), R) --> true
+end
